9bb90b61a26842426f94f17f0e8a413ee091636d
Suhani Mishra <suhani1657a@gmail.com>
2025-03-03 21:43:58 +0530

    Right Angle Number Pyramid in C++

--- Patterns/RightAngleNoPyramid.cpp
+++ Patterns/RightAngleNoPyramid.cpp
@@ -6,7 +6,9 @@ int main(){
     //taking input from user
     cin>>n;
 
+    //outer loop for rows
     for(int i=1; i<=n; i++){
+        //inner loop for cols 
         for(int j=1; j<=i; j++){
             cout << j;
         }
