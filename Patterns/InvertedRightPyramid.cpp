b100d6efd56075f126e0053337b28f2dca8d279b
Suhani Mishra <suhani1657a@gmail.com>
2025-03-03 21:19:26 +0530

    Inverted Right Pyramid in C++

--- Patterns/InvertedRightPyramid.cpp
+++ Patterns/InvertedRightPyramid.cpp
@@ -6,7 +6,10 @@ int main(){
     //taking input from user
     cin>>n;
 
+    //outer loop for the rows
     for(int i=0; i<=n; i++){
+        //inner loop for the cols
+        //no. of col = n-i
         for(int j=1; j<=n-i; j++){
             cout << "*";
         }
