f8a54639ce02c3870eb09477f5d0e2862805070c
Suhani Mishra <suhani1657a@gmail.com>
2025-03-12 22:55:09 +0530

    Searching and Sorting Algorithms

--- SearchingAndSorting/quickSort.cpp
+++ SearchingAndSorting/quickSort.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 
 int partition(vector<int> &arr, int low, int high){
-    int pivot = arr[low];
-    int i = low;
-    int j = high;
+    int pivot = arr[low]; //The pivot is chosen as the first element (arr[low])
+    int i = low; //i is initialized to low
+    int j = high; //j is initialized to high
     while(i < j){
         while(arr[i] <= pivot && i <= high-1){
             i++;
@@ -13,10 +13,14 @@ int partition(vector<int> &arr, int low, int high){
         while(arr[j] > pivot && j >= low+1){
             j--;
         }
+
+        // If i < j, it means two elements are out of order, so we swap them.
         if(i < j) swap(arr[i],arr[j]);
     }
+
+    //swaps the pivot (arr[low]) with arr[j] so that it is correctly positioned.
     swap(arr[low],arr[j]);
-    return j;
+    return j; //arr[j] is now the correct partition index.
 }
 
 void QuickSort(vector<int> &arr, int low, int high){
